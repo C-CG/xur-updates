951a7effa2d08f90e7e721875c6cb7fa94fc9f56
Calvin-Castle Gill <calvincgill@gmail.com>
2019-08-17 15:21:54 +0100

    Main function (returnItems) now takes in props, meaning that we can pass each item ID to it as well as name etc for easy access.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
+import items from '../../BungieManifest/InventoryItem.json';
 
 const url = 'https://www.bungie.net/Platform/Destiny2//Vendors/?components=402';
-const comp = ''
 const apiKey = '39c9bfabfc4848469e52019b7115052c'
 
 class xurUpdates extends React.Component {
@@ -24,6 +24,7 @@ class xurUpdates extends React.Component {
     .then(
       (result) => {
         this.setState({
+          // Large ID = unique reference to Xur
           exotics: result.Response.sales.data["2190858386"].saleItems
         });
       },
@@ -32,14 +33,29 @@ class xurUpdates extends React.Component {
           error
         });
       }
-    )
-    
+    )   
     // Loading Screen
     setTimeout(
       function() {
         this.setState({loaded : true});
       }.bind(this), 2000
     )
+  }
+
+  returnItems(props) {
+    var obj = Object.values(items); 
+    for (var i = 0; i < obj.length; i++) {
+      if(obj[i].hash == props.item) {
+        console.log("ITEM FOUND: " + obj[i].displayProperties.name)
+        return(
+          <div>
+            <p>{obj[i].displayProperties.name}</p>
+            <img src={"https://www.bungie.net" + obj[i].displayProperties.icon}/>
+          </div>
+        )    
+      }
+    }
+    return(<div>NOT FOUND</div>)
   }
 
   render() {
@@ -66,11 +82,11 @@ class xurUpdates extends React.Component {
           <ul>
             {array.map(item => (
               <li key={item.key}>
-                <p>{item.itemHash}</p>
+                <this.returnItems item={item.itemHash}/>
               </li>
             ))
             }
-          </ul>
+          </ul>      
         </div>
         )
     } 
